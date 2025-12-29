a86ab2cc2b12b633f6da4abd66c0d16c3b0d3abc
Nakamurakei2 <ktyun2525@gmail.com>
2025-12-30 04:45:48 +0900

    feat: create contact page(TODO)

--- calendar-share/pages/api/users.ts
+++ calendar-share/pages/api/users.ts
@@ -1,14 +1,14 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import pool from "../../lib/db";
-import { UserResponse } from "../chat/types";
+import {NextApiRequest, NextApiResponse} from 'next';
+import pool from '../../lib/db';
+import {UserResponse} from '../chat/types';
 
-
-export default async function handler (req: NextApiRequest, res: NextApiResponse<UserResponse[]>) {
-  if(req.method === 'GET') {
-    const result = await pool.query(
-      'select name from users'
-    )
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<UserResponse[]>,
+) {
+  if (req.method === 'GET') {
+    const result = await pool.query('select name from users');
     const names: UserResponse[] = result.rows;
-    res.status(200).json(names)
+    res.status(200).json(names);
   }
 }
