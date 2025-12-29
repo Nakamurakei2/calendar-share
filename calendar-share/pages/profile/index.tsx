0a8de62a16f6475701b4542dac55f1ccae283c25
Nakamurakei2 <ktyun2525@gmail.com>
2025-12-29 17:35:32 +0900

    fix: css, jsx

--- calendar-share/pages/profile/index.tsx
+++ calendar-share/pages/profile/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Footer from '../../src/components/ui/footer/Footer';
 import ProfileHeader from '../../src/components/ui/ProfileHeader/ProfileHeader';
 import {useFooterActions} from '../../src/hooks/useFooterActions';
@@ -10,6 +11,24 @@ const ProfileIndexPage = () => {
   return (
     <div className={styles.profileWrapper}>
       <ProfileHeader />
+
+      <div className={styles.profileContainer}>
+        <p className={styles.profileSubtitle}>基本情報</p>
+
+        <div className={styles.profileContent}>
+          <p className={styles.title}>電話番号</p>
+          <p className={styles.value}>090-0000-0000</p>
+        </div>
+
+        <div className={styles.profileContent}>
+          <p className={styles.title}>名前</p>
+          <p className={styles.value}>demo中村</p>
+        </div>
+      </div>
+
+      <p className={styles.contactLink}>
+        お問い合わせは <Link href="/contact">こちら</Link>から
+      </p>
       <Footer
         onCalendarBtnClick={onCalendarBtnClick}
         onChatBtnClick={onChatBtnClick}
--- calendar-share/src/components/ui/ProfileHeader/ProfileHeader.tsx
+++ calendar-share/src/components/ui/ProfileHeader/ProfileHeader.tsx
@@ -1,28 +1,9 @@
-import Link from 'next/link';
 import styles from './styles.module.css';
 
 const ProfileHeader = () => {
   return (
     <div className={styles.profileWrapper}>
-      <p className={styles.profileHeader}>設定</p>
-
-      <div className={styles.profileContainer}>
-        <p className={styles.profileSubtitle}>基本情報</p>
-
-        <div className={styles.profileContent}>
-          <p className={styles.title}>電話番号</p>
-          <p className={styles.value}>090-0000-0000</p>
-        </div>
-
-        <div className={styles.profileContent}>
-          <p className={styles.title}>名前</p>
-          <p className={styles.value}>demo中村</p>
-        </div>
-      </div>
-
-      <p className={styles.contactLink}>
-        お問い合わせは <Link href="/contact">こちら</Link>から
-      </p>
+      <p className={styles.profileTitle}>設定</p>
     </div>
   );
 };
