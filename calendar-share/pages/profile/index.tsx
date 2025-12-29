808b74900f0fff317f6c92f5f825e34734393c60
Nakamurakei2 <ktyun2525@gmail.com>
2025-12-29 16:53:15 +0900

    feat: create template profile page

--- calendar-share/pages/profile/index.tsx
+++ calendar-share/pages/profile/index.tsx
@@ -1,7 +1,22 @@
+import Footer from '../../src/components/ui/footer/Footer';
+import ProfileHeader from '../../src/components/ui/ProfileHeader/ProfileHeader';
+import {useFooterActions} from '../../src/hooks/useFooterActions';
 import styles from './styles.module.css';
 
 const ProfileIndexPage = () => {
-  return <div>profile index</div>;
+  const {onCalendarBtnClick, onChatBtnClick, onProfileBtnClick} =
+    useFooterActions();
+
+  return (
+    <div className={styles.profileWrapper}>
+      <ProfileHeader />
+      <Footer
+        onCalendarBtnClick={onCalendarBtnClick}
+        onChatBtnClick={onChatBtnClick}
+        onProfileBtnClick={onProfileBtnClick}
+      />
+    </div>
+  );
 };
 
 export default ProfileIndexPage;
